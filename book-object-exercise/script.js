8f4c5ebc1aadf153384de6883c6fdebf33eab55d
BukolaOmok <157320642+BukolaOmok@users.noreply.github.com>
2024-04-03 20:22:06 +0100

    add player practice + changed prototype info from using + for joining words to the ${

--- book-object-exercise/script.js
+++ book-object-exercise/script.js
@@ -6,7 +6,7 @@ function Book(title, author, pages, readStatus) {
 }
 
 Book.prototype.info = function () {
-    return this.title + " by " + this.author + ', ' + this.pages + ", " + this.readStatus
+    return (`${this.title} by ${this.author},  ${this.pages}, ${this.readStatus}`)
 }
 
 let book1 = new Book("Let Me In", "Claire McGowan", 500, "read");
@@ -15,3 +15,22 @@ console.log(bookInfo);
 
 
 
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+  this.sayName = function() {
+    console.log(this.name)
+  };
+}
+
+const player1 = new Player('steve', 'X');
+const player2 = new Player('also steve', 'O');
+player1.sayName(); // logs 'steve'
+player2.sayName(); // logs 'also steve'
+
+Player.prototype.sayHello = function() {
+   console.log("Hello, I'm a player!");
+};
+
+player1.sayHello(); // logs "Hello, I'm a player!"
+player2.sayHello(); // logs "Hello, I'm a player!"
